6d7862e7154b414274efdfff1d607af545498810
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-09-11 01:04:45 +0530

    p7

--- P7/__init__.py
+++ P7/__init__.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template
 p7_bp = Blueprint('p7', __name__, template_folder='templates', static_folder='static')
 
 # Define the route for the index page of Practical 7
-@p7_bp.route('/')
+@p7_bp.route('/p7')  # Changed route to match the unique prefix
 def p7_index():
-    # Render the appropriate template (adjust template name if needed)
+    # Render the appropriate template
     return render_template('index.html')
