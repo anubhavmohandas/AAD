8c88c20dcb5522eda09da06faacf89850958332d
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-09-11 00:54:21 +0530

    p7

--- P7/__init__.py
+++ P7/__init__.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template
 
+# Define the blueprint for Practical 7
 p7_bp = Blueprint('p7', __name__, template_folder='templates', static_folder='static')
 
-@p7_bp.route('/p7')
-def p7():
+# Define the route for the index page of Practical 7
+@p7_bp.route('/')
+def p7_index():
+    # Render the appropriate template (adjust template name if needed)
     return render_template('index.html')
