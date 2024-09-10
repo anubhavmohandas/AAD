2b2a022b7324b94b1aeb44bf3e50c868bdba68d1
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-09-11 00:13:52 +0530

    update p2

--- P2/practical/__init__.py
+++ P2/practical/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 practical_bp = Blueprint('practical', __name__, template_folder='templates', static_folder='static')
 
-@practical_bp.route('/')
+@practical_bp.route('/practical')
 def index():
     return render_template('index.html')
