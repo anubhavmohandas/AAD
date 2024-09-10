69a5c4fb119a30cae3e53afd50076bbb21f8cce5
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-09-11 01:11:13 +0530

    p7 /

--- P7/__init__.py
+++ P7/__init__.py
@@ -4,6 +4,6 @@ from flask import Blueprint, render_template
 p7_bp = Blueprint('p7', __name__, template_folder='templates', static_folder='static')
 
 # Define the route for the index page of Practical 7
-@p7_bp.route('/p7')
+@p7_bp.route('/')  # Correct the route to align with the prefix
 def p7_index():
-    return render_template('index.html')  
+    return render_template('index.html')  # Ensure this points to the correct index.html in the templates folder of P7
