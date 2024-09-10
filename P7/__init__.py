31daa14dc3ec768560b228a315e8506d29752e2c
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-09-11 00:36:51 +0530

    p7

--- P7/__init__.py
+++ P7/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 p7_bp = Blueprint('p7', __name__, template_folder='templates', static_folder='static')
 
-@p7_bp.route('/app')
-def app():
+@p7_bp.route('/p7')
+def p7():
     return render_template('index.html')
